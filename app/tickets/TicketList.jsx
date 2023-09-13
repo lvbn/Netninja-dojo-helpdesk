ca4cfeee2ce91cf588aa40832a443e54f09be26f
lvbn <luiz.neto.v@gmail.com>
2023-09-12 21:58:28 -0300

    [created TicketDetails component][][]

--- app/tickets/TicketList.jsx
+++ app/tickets/TicketList.jsx
@@ -1,7 +1,9 @@
+import Link from "next/link"
+
 async function getTickets() {
   const res = await fetch('http://localhost:4000/tickets', {
     next: {
-      revalidate: 0
+      revalidate: 0 // use 0 to opt out of using cache
     }
   })
 
@@ -15,11 +17,13 @@ export default async function TicketList() {
     <>
       {tickets && tickets.map((ticket) => (
         <div key={ticket.id} className='card my-5'>
-          <h3>{ticket.title}</h3>
-          <p>{ticket.body.slice(0, 200)}...</p>
-          <div className={`pill ${ticket.priority}`}>
-            {ticket.priority} priority
-          </div>
+          <Link href={`/tickets/${ticket.id}`}>
+            <h3>{ticket.title}</h3>
+            <p>{ticket.body.slice(0, 200)}...</p>
+            <div className={`pill ${ticket.priority}`}>
+              {ticket.priority} priority
+            </div>
+          </Link>
         </div>
       ))}
       {!tickets && (
--- app/tickets/[id]/page.jsx
+++ app/tickets/[id]/page.jsx
@@ -1,9 +1,31 @@
+async function getTicket(id) {
+  const res = await fetch('http://localhost:4000/tickets/' + id, {
+    next: {
+      revalidate: 60 // use 0 to opt out of using cache
+    }
+  })
+
+  return res.json()
+}
+
 import React from 'react'
 
-export default function TicketDetails({ params }) {
-  const id = params.id
+export default async function TicketDetails({ params }) {
+  const ticket = await getTicket(params.id)
 
   return (
-    <div>{id}</div>
+    <main>
+      <nav>
+        <h2>Ticket Details</h2>
+      </nav>
+      <div className='card'>
+        <h3>{ticket.title}</h3>
+        <small>Created by {ticket.user_email}</small>
+        <p>{ticket.body}</p>
+        <div className={`pill ${ticket.priority}`}>
+          {ticket.priority} priority
+        </div>
+      </div>
+    </main>
   )
 }
